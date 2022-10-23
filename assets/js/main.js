deb97114ee9eb65167313c0b436220c8c48c0c19
TheRWProjects <100331812+ourstorycomic@users.noreply.github.com>
2022-10-23 21:48:28 +0700

    assets upload

---
+++ assets/js/main.js
@@ -0,0 +1,492 @@
+(function ($) {
+	"use strict";
+
+/*=============================================
+	=    		 Preloader			      =
+=============================================*/
+function preloader() {
+	$('#preloader').delay(0).fadeOut();
+};
+
+$(window).on('load', function () {
+	preloader();
+	mainSlider();
+	mainSliderTwo();
+	wowAnimation();
+});
+
+
+
+/*=============================================
+	=    		Mobile Menu			      =
+=============================================*/
+//SubMenu Dropdown Toggle
+if ($('.menu-area li.menu-item-has-children ul').length) {
+	$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
+}
+
+//Mobile Nav Hide Show
+if ($('.mobile-menu').length) {
+
+	var mobileMenuContent = $('.menu-area .main-menu').html();
+	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+
+	//Dropdown Button
+	$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
+		$(this).toggleClass('open');
+		$(this).prev('ul').slideToggle(500);
+	});
+	//Menu Toggle Btn
+	$('.mobile-nav-toggler').on('click', function () {
+		$('body').addClass('mobile-menu-visible');
+	});
+
+	//Menu Toggle Btn
+	$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
+		$('body').removeClass('mobile-menu-visible');
+	});
+}
+
+
+/*=============================================
+	=     Data Background      =
+=============================================*/
+$("[data-background").each(function () {
+	$(this).css("background-image", "url( " + $(this).attr("data-background") + "  )");
+});
+
+
+/*=============================================
+	=     Menu sticky & Scroll to top      =
+=============================================*/
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$("#sticky-header").removeClass("sticky-menu");
+		$('.scroll-to-target').removeClass('open');
+
+	} else {
+		$("#sticky-header").addClass("sticky-menu");
+		$('.scroll-to-target').addClass('open');
+	}
+});
+
+
+/*=============================================
+	=    		 Scroll Up  	         =
+=============================================*/
+if ($('.scroll-to-target').length) {
+  $(".scroll-to-target").on('click', function () {
+    var target = $(this).attr('data-target');
+    // animate
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1000);
+
+  });
+}
+
+
+/*=============================================
+	=            Header Search            =
+=============================================*/
+$(".header-search > a").on('click', function () {
+	$(".search-popup-wrap").slideToggle();
+	return false;
+});
+
+$(".search-close").on('click', function () {
+	$(".search-popup-wrap").slideUp(500);
+});
+
+
+/*=============================================
+	=    		Movie Active  	         =
+=============================================*/
+$('.coming-movie-item').on('mouseenter', function () {
+	$(this).addClass('active').parent().siblings().find('.coming-movie-item').removeClass('active');
+})
+
+
+
+/*=============================================
+	=    		 Main Slider		      =
+=============================================*/
+function mainSlider() {
+	if (jQuery(".slider-active").length > 0) {
+		let sliderActive1 = ".slider-active";
+		let sliderInit1 = new Swiper(sliderActive1, {
+			// Optional parameters
+			slidesPerView: 1,
+			slidesPerColumn: 1,
+			loop: true,
+			effect: 'fade',
+			autoplay: {
+				delay: 10000,
+			},
+
+			// If we need pagination
+			pagination: {
+				el: ".slider-pagination",
+				// dynamicBullets: true,
+				clickable: true,
+			},
+
+			// Navigation arrows
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+
+			a11y: false,
+		});
+
+		function animated_swiper(selector, init) {
+			let animated = function animated() {
+				$(selector + " [data-animation]").each(function () {
+					let anim = $(this).data("animation");
+					let delay = $(this).data("delay");
+					let duration = $(this).data("duration");
+
+					$(this)
+						.removeClass("anim" + anim)
+						.addClass(anim + " animated")
+						.css({
+							webkitAnimationDelay: delay,
+							animationDelay: delay,
+							webkitAnimationDuration: duration,
+							animationDuration: duration,
+						})
+						.one(
+							"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+							function () {
+								$(this).removeClass(anim + " animated");
+							}
+						);
+				});
+			};
+			animated();
+			// Make animated when slide change
+			init.on("slideChange", function () {
+				$(sliderActive1 + " [data-animation]").removeClass("animated");
+			});
+			init.on("slideChange", animated);
+		}
+
+		animated_swiper(sliderActive1, sliderInit1);
+	}
+};
+
+/*=============================================
+	=    		 Main Slider		      =
+=============================================*/
+function mainSliderTwo() {
+	var BasicSlider = $('.slider-active-two');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: true,
+		autoplaySpeed: 10000,
+		dots: false,
+		fade: true,
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-prev"><i class="flaticon-next"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="flaticon-next"></i></button>',
+		appendArrows: ".slider-nav",
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+};
+
+/*=============================================
+	=    		Movie Active		      =
+=============================================*/
+$('.coming-movie-active').slick({
+	dots: false,
+	infinite: true,
+	speed: 600,
+	autoplay: true,
+	arrows: false,
+	slidesToShow: 6,
+	slidesToScroll: 2,
+	responsive: [
+		{
+			breakpoint: 1400,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 575,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+	]
+});
+$('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+	$('.coming-movie-active').slick('refresh');
+});
+
+
+/*=============================================
+	=      TSD Active 	      =
+=============================================*/
+$('.tsd-poster-active').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	dots: false,
+	asNavFor: '.tsd-nav-active',
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				arrows: false,
+				dots: false,
+			}
+		},
+	]
+});
+$('.tsd-nav-active').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.tsd-poster-active',
+	dots: false,
+	arrows: true,
+	centerMode: false,
+	centerPadding: '0px',
+	prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-up"></i></button>',
+	nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-angle-down"></i></button>',
+	vertical: true,
+	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				vertical: false,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				arrows: false,
+				vertical: false,
+			}
+		},
+		{
+			breakpoint: 575,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				vertical: false,
+			}
+		},
+	]
+});
+
+/*=============================================
+	=         Banner-movie-active           =
+=============================================*/
+$('.banner-movie-active').slick({
+	centerMode: true,
+	autoplay: true,
+	infinite: true,
+	arrows: false,
+	speed: 500,
+	centerPadding: '0',
+	slidesToShow: 1,
+	responsive: [
+		{
+			breakpoint: 1800,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 1500,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerPadding: '30px',
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerPadding: '50px',
+				infinite: true,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+				centerPadding: '0',
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerPadding: '0px',
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 575,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerPadding: '0px',
+				arrows: false,
+			}
+		},
+	]
+});
+$('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+	$('.banner-movie-active').slick('refresh');
+});
+
+/*=============================================
+	=    		Odometer Active  	       =
+=============================================*/
+$('.odometer').appear(function (e) {
+	var odo = $(".odometer");
+	odo.each(function () {
+		var countNumber = $(this).attr("data-count");
+		$(this).html(countNumber);
+	});
+});
+
+
+/*=============================================
+	=    		Magnific Popup		      =
+=============================================*/
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+		enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+/*=============================================
+	=    		Isotope	Active  	      =
+=============================================*/
+$('.latest-movie-active').imagesLoaded(function () {
+	// init Isotope
+	var $grid = $('.latest-movie-active').isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-item',
+		}
+	});
+	// filter items on button click
+	$('.latest-movie-nav').on('click', 'button', function () {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+
+});
+//for menu active class
+$('.latest-movie-nav button').on('click', function (event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+/*=============================================
+	=    		 Wow Active  	         =
+=============================================*/
+function wowAnimation() {
+	var wow = new WOW({
+		boxClass: 'wow',
+		animateClass: 'animated',
+		offset: 0,
+		mobile: false,
+		live: true
+	});
+	wow.init();
+}
+
+
+})(jQuery);
